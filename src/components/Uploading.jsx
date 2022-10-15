4557e318e98f73d3cb22d106b3333a64dbfe304c
based-on-what <ignacio.pereira@uc.cl>
2022-10-15 17:53:33 -0300

    need to merge encoding component and original input

--- src/components/Uploading.jsx
+++ src/components/Uploading.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import languageEncoding from "detect-file-encoding-and-language";
 
 function FileUploadPage(){
 
@@ -10,7 +11,11 @@ function FileUploadPage(){
 	const changeHandler = (event) => {
 		setSelectedFile(event.target.files[0]);
 		setIsSelected(true);
-	};
+        languageEncoding(selectedFile).then(
+            (fileInfo) => console.log(Object.values(fileInfo)[0])
+            );
+
+    };
 
 	const handleSubmission = (file) => {
         fileReader = new FileReader();
@@ -22,11 +27,12 @@ function FileUploadPage(){
         <div>
         <input type="file" name="file" onChange={changeHandler} />
         {isSelected ? (
-            
+    
             <div>
                 <p>Filename: {selectedFile.name}</p>
                 <p>Filetype: {selectedFile.type}</p>
-                <p>Size: {selectedFile.size} Bytes</p>       
+                <p>Size: {selectedFile.size} Bytes</p>
+                <p>Encoding: {selectedFile.info} </p>       
             </div>
         ) : (
             <p>Select a file to show details</p>
