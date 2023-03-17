00bbcc509f98ec593f269f93b57b25f509b784fd
VicenteVicente <37163344+VicenteVicente@users.noreply.github.com>
2023-03-17 12:46:53 +0000

    Deploying to gh-pages from @ REmatchChile/REmatch-web@b98e9383f99a6e577517f5fdc08956d62492dd05 🚀

---
+++ precache-manifest.146b850acb02f76b3189566fa74c775f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "54fe523f20e73bd9cc2b",
+    "url": "/REmatch-web/static/css/main.e737715c.chunk.css"
+  },
+  {
+    "revision": "54fe523f20e73bd9cc2b",
+    "url": "/REmatch-web/static/js/main.54fe523f.chunk.js"
+  },
+  {
+    "revision": "9a45ed9ecff4bdc59237",
+    "url": "/REmatch-web/static/css/1.c027952a.chunk.css"
+  },
+  {
+    "revision": "9a45ed9ecff4bdc59237",
+    "url": "/REmatch-web/static/js/1.9a45ed9e.chunk.js"
+  },
+  {
+    "revision": "518a3e6347b7dfc2f398",
+    "url": "/REmatch-web/static/js/runtime~main.518a3e63.js"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/REmatch-web/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/REmatch-web/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/REmatch-web/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/REmatch-web/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/REmatch-web/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/REmatch-web/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/REmatch-web/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/REmatch-web/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/REmatch-web/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/REmatch-web/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/REmatch-web/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/REmatch-web/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/REmatch-web/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/REmatch-web/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/REmatch-web/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/REmatch-web/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "ba51fa3aaf3f323421fea14192cdf6f8",
+    "url": "/REmatch-web/index.html"
+  }
+];
