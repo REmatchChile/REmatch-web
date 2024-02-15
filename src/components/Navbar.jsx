d29b80c09c3a82b6a8a3ebe8d954b74c8243e376
VicenteVicente <vicente.calisto98@gmail.com>
2024-02-15 17:44:20 -0300

    small changes, github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
-
+import GitHubIcon from "@mui/icons-material/GitHub";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import { Link } from "react-router-dom";
 
 import Logo from "../assets/logo-dark.png";
 
-export default function NavbarComponent() {
+export default function NavbarComponent({ themeMode, toggleThemeMode }) {
   return (
     <AppBar position="fixed">
       <Toolbar>
@@ -26,6 +28,18 @@ export default function NavbarComponent() {
             }}
           />
         </Link>
+        <Box sx={{ flexGrow: 1 }} />
+        <Tooltip title="GitHub">
+          <IconButton
+            size="large"
+            edge="end"
+            href="https://github.com/REmatchChile"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHubIcon />
+          </IconButton>
+        </Tooltip>
       </Toolbar>
     </AppBar>
   );
