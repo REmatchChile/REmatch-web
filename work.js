a411efd84730421eeebc9a05f71293626da5b680
VicenteVicente <37163344+VicenteVicente@users.noreply.github.com>
2024-03-21 13:55:17 +0000

    Deploying to gh-pages from @ REmatchChile/REmatch-web@c4108a0cf0d1d5bbaf4bd54f938c0b56900d6a78 🚀

--- work.js
+++ work.js
@@ -1,12 +1,16 @@
 import REmatch from "./emscripten_binding.js";
 
 // Number of iterations before posting a matches message
-const MIN_MATCHES_PER_POST = 15000;
+const MIN_MATCHES_PER_POST = 8192;
 
 // Initialize WASM
 let REmatchModuleInstance = null;
 (async () => {
   REmatchModuleInstance = await REmatch();
+  REmatchModuleInstance.onAbort = () => {
+    // Abort event is triggered on critical errors, for example when the worker runs out of memory
+    self.postMessage({ type: "ABORT" });
+  };
   self.postMessage({ type: "ALIVE" });
 })();
 
@@ -19,52 +23,63 @@ const getErrorText = (error) => {
 };
 
 let queryId;
+let isMultiRegex;
 let variables;
 let regex;
 let match_iterator;
 
 self.onmessage = (event) => {
-  switch (event.data.type) {
-    case "QUERY_INIT": {
-      queryId = event.data.queryId;
-      // Initialize query
-      variables = [];
-      if (regex) {
-        regex.delete();
-        regex = null;
-      }
-      if (match_iterator) {
-        match_iterator.delete();
-        match_iterator = null;
-      }
-      try {
-        regex = new REmatchModuleInstance.Regex(event.data.query);
+  try {
+    switch (event.data.type) {
+      case "QUERY_INIT": {
+        queryId = event.data.queryId;
+        isMultiRegex = event.data.isMultiRegex;
+        // Initialize query
+        variables = [];
+        if (regex) {
+          regex.delete();
+          regex = null;
+        }
+        if (match_iterator) {
+          match_iterator.delete();
+          match_iterator = null;
+        }
+
+        if (isMultiRegex) {
+          regex = new REmatchModuleInstance.MultiRegex(event.data.query);
+        } else {
+          regex = new REmatchModuleInstance.Regex(event.data.query);
+        }
         match_iterator = regex.finditer(event.data.doc);
         // Get variables
-        const variables_vector = match_iterator.variables();
-        for (let i = 0; i < variables_vector.size(); ++i)
-          variables.push(variables_vector.get(i));
+        const variablesVector = match_iterator.variables();
+        for (let i = 0; i < variablesVector.size(); ++i)
+          variables.push(variablesVector.get(i));
+        variablesVector.delete();
         self.postMessage({ type: "QUERY_VARIABLES", variables, queryId });
-      } catch (err) {
-        self.postMessage({
-          type: "ERROR",
-          error: getErrorText(err),
-        });
+        break;
       }
-      break;
-    }
-    case "QUERY_NEXT": {
-      // Send a chunk of matches, notifying the main thread if there are more matches to send
-      try {
+      case "QUERY_NEXT": {
+        // Send a chunk of matches, notifying the main thread if there are more matches to send
         let match = match_iterator.next();
         const matches = [];
         while (match != null) {
-          matches.push(
-            variables.map((variable) => [
-              match.start(variable),
-              match.end(variable),
-            ])
-          );
+          const matchData = variables.map((variable) => {
+            if (isMultiRegex) {
+              const spansVector = match.spans(variable);
+              const spans = [];
+              for (let i = 0; i < spansVector.size(); ++i) {
+                const [from, to] = spansVector.get(i);
+                // Need to convert from BigInt to Number in order to work with the application}
+                spans.push([Number(from), Number(to)]);
+              }
+              return spans;
+            } else {
+              const [from, to] = match.span(variable);
+              return [[Number(from), Number(to)]];
+            }
+          });
+          matches.push(matchData);
           // Post matches chunk and stop execution until further requests
           if (matches.length >= MIN_MATCHES_PER_POST) {
             self.postMessage({
@@ -84,18 +99,17 @@ self.onmessage = (event) => {
           hasNext: false,
           queryId,
         });
-      } catch (err) {
-        self.postMessage({
-          type: "ERROR",
-          error: getErrorText(err),
-          queryId,
-        });
+        break;
       }
-      break;
+      default: {
+        console.error("UNHANDLED WORKER MESSAGE SENT", event.data);
+        break;
+      }
     }
-    default: {
-      console.error("UNHANDLED WORKER MESSAGE SENT", event.data);
-      break;
-    }
+  } catch (err) {
+    self.postMessage({
+      type: "ERROR",
+      error: getErrorText(err),
+    });
   }
 };
