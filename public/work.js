5c6264dc4fb2146c72f137e1c7bea430bc9516f5
VicenteVicente <vicente.calisto98@gmail.com>
2024-09-01 17:41:15 -0300

    updated with rematch-javascript

--- public/work.js
+++ public/work.js
@@ -1,84 +1,63 @@
-import REmatch from "./emscripten_binding.js";
+import initREmatch from "https://cdn.jsdelivr.net/npm/rematch-javascript@1.1.1/lib/rematch.esm.min.js";
 
 // Number of iterations before posting a matches message
 const MIN_MATCHES_PER_POST = 8192;
 
 // Initialize WASM
-let REmatchModuleInstance = null;
+let REmatch = null;
 (async () => {
-  REmatchModuleInstance = await REmatch();
-  REmatchModuleInstance.onAbort = () => {
+  REmatch = await initREmatch();
+  REmatch.setOnAbort(() => {
     // Abort event is triggered on critical errors, for example when the worker runs out of memory
     self.postMessage({ type: "ABORT" });
-  };
+  });
   self.postMessage({ type: "ALIVE" });
 })();
 
+// TODO:
 const getErrorText = (error) => {
   if (error instanceof WebAssembly.Exception) {
-    const [type, message] = REmatchModuleInstance.getExceptionMessage(error);
+    const [type, message] = REmatch.getExceptionMessage(error);
     return `${type}: ${message}`;
   }
   return error.toString();
 };
 
 let queryId;
-let isMultiMatch;
+let query;
 let variables;
 let regex;
-let match_iterator;
+let matchIterator;
+let multiMatch;
 
 self.onmessage = (event) => {
   try {
     switch (event.data.type) {
       case "QUERY_INIT": {
         queryId = event.data.queryId;
-        isMultiMatch = event.data.isMultiMatch;
+        multiMatch = event.data.isMultiMatch;
         // Initialize query
-        variables = [];
         if (regex) {
-          regex.delete();
+          regex.free();
           regex = null;
         }
-        if (match_iterator) {
-          match_iterator.delete();
-          match_iterator = null;
+        if (matchIterator) {
+          matchIterator.free();
+          matchIterator = null;
         }
 
-        if (isMultiMatch) {
-          regex = new REmatchModuleInstance.MultiRegex(event.data.query);
-        } else {
-          regex = new REmatchModuleInstance.Regex(event.data.query);
-        }
-        match_iterator = regex.finditer(event.data.doc);
+        query = REmatch.reql(event.data.query, { multiMatch });
+        matchIterator = query.findIter(event.data.doc);
         // Get variables
-        const variablesVector = match_iterator.variables();
-        for (let i = 0; i < variablesVector.size(); ++i)
-          variables.push(variablesVector.get(i));
-        variablesVector.delete();
+        variables = matchIterator.variables();
         self.postMessage({ type: "QUERY_VARIABLES", variables, queryId });
         break;
       }
       case "QUERY_NEXT": {
         // Send a chunk of matches, notifying the main thread if there are more matches to send
-        let match = match_iterator.next();
         const matches = [];
-        while (match != null) {
-          const matchData = variables.map((variable) => {
-            if (isMultiMatch) {
-              const spansVector = match.spans(variable);
-              const spans = [];
-              for (let i = 0; i < spansVector.size(); ++i) {
-                const [from, to] = spansVector.get(i);
-                // Need to convert from BigInt to Number in order to work with the application}
-                spans.push([Number(from), Number(to)]);
-              }
-              return spans;
-            } else {
-              const [from, to] = match.span(variable);
-              return [[Number(from), Number(to)]];
-            }
-          });
+        for (const match of matchIterator) {
+          const matchData = variables.map((variable) => (multiMatch ? match.spans(variable) : [match.span(variable)]));
           matches.push(matchData);
           // Post matches chunk and stop execution until further requests
           if (matches.length >= MIN_MATCHES_PER_POST) {
@@ -90,8 +69,10 @@ self.onmessage = (event) => {
             });
             return;
           }
-          match = match_iterator.next();
+
+          match.free();
         }
+
         // Last chunk of matches, it may be empty but it is necessary to notify the main thread
         self.postMessage({
           type: "QUERY_NEXT",
